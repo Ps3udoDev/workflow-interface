dcc530f11393d3eafbb62c3e221d2f21c6994c32
Pseudo <v.pseudo.developer@gmail.com>
2023-09-17 16:56:54 -0500

    feat(app): Create basic structure and add flowPanel component
    An initial restructure of the application has been performed to create the basic structure. Additionally, the flowPanel component has been added which will be essential for the flow of the application.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import {ReactFlowProvider} from 'reactflow'
+import FlowPanel from './components/reactFlow/FlowPanel'
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='bg-[#1c1d1f]'>
+      <ReactFlowProvider>
+        <div className='border-2 border-blue-600 m-auto items-center justify-center w-full h-screen'>
+          <FlowPanel />
+        </div>
+      </ReactFlowProvider>
+    </div>
     </>
   )
 }
---
+++ src/components/reactFlow/FlowPanel.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FlowPanel = () => {
+  return (
+    <div>FlowPanel</div>
+  )
+}
+
+export default FlowPanel
