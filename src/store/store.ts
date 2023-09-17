6d160fc9171e15cc4d8d46e37bbf2b9af250eed8
Pseudo <v.pseudo.developer@gmail.com>
2023-09-17 17:02:48 -0500

    feat(store): Create store to manage values of nodes and edges
    A store has been implemented with the help of zustand that will allow efficient management and access to the values of nodes and edges in the application.

---
+++ src/store/store.ts
@@ -0,0 +1,50 @@
+import { create } from 'zustand'
+
+import {
+  Connection,
+  Edge,
+  EdgeChange,
+  Node,
+  NodeChange,
+  addEdge,
+  OnNodesChange,
+  OnEdgesChange,
+  OnConnect,
+  applyNodeChanges,
+  applyEdgeChanges,
+} from 'reactflow'
+
+import initialNodes from './nodes'
+import initialEdges from './edges'
+
+
+type RFState = {
+  nodes: Node[];
+  edges: Edge[];
+  onNodesChange: OnNodesChange;
+  onEdgesChange: OnEdgesChange;
+  onConnect: OnConnect;
+}
+
+const useStore = create<RFState>((set, get) => ({
+  nodes: initialNodes,
+  edges: initialEdges,
+  onNodesChange: (changes: NodeChange[]) => {
+    set({
+      nodes: applyNodeChanges(changes, get().nodes),
+    });
+  },
+  onEdgesChange: (changes: EdgeChange[]) => {
+    set({
+      edges: applyEdgeChanges(changes, get().edges),
+    });
+  },
+  onConnect: (connection: Connection) => {
+    set({
+      edges: addEdge(connection, get().edges),
+    });
+  },
+}))
+
+
+export default useStore;
